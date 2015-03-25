2e068d66ee34ac4bd27c10bfbc1276dbd632ccc0
jeremy <jeremymartinez11@gmail.com>
2015-03-24 22:18:33 -0700

    changed config file

--- config.sh
+++ config.sh
@@ -1,5 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install git
+
+#ssh key configuration
+mkdir ~/.ssh
+ssh-keygen -t rsa -C "jeremymartinez11@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+#dependencies for rails app
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
+#ruby installation
 cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
@@ -16,6 +29,7 @@ rbenv global 2.1.5
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 gem install bundler
 
+#rails installation
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
@@ -23,3 +37,7 @@ sudo apt-get install nodejs
 gem install rails -v 4.2.0
 
 rbenv rehash
+
+git clone git@github.com:JeremyCraigMartinez/421_WEBAPP.git
+
+bundle install
