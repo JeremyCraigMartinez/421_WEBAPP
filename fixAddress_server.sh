2eefb219f0914726be5b9e47e5327cef0e31ad1e
James Jessen <jsjessen@gmail.com>
2015-04-23 00:13:05 -0700

    Add minor improvements to fix address script

--- fixAddress_server.sh
+++ fixAddress_server.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-location="~/421_WEBAPP/myapp/app/assets/javascripts/app/services"
-ext="js"
+location="~/421_WEBAPP/myapp/app/assets/javascripts/app/services" # consider using '.'
+ext="js" #consider using '*'
 localAddress="localhost"
 serverAddress="104.236.169.12"
 
+echo "Listing locations of '$localAddress'"
+find $location -name "*.$ext" -print
+echo
 
 echo "Changing $localAddress to $serverAddress recursively in '$location'"
-
 find $location -name "*.$ext" -print | xargs sed -i "s/$localAddress/$serverAddress/g"
 
 if [ $? -eq 0 ]; then
@@ -15,3 +17,4 @@ if [ $? -eq 0 ]; then
 else
     echo "FAILED"
 fi
+echo
