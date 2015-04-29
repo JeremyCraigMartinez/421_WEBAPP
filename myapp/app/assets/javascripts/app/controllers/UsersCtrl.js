55200eb1428792952eeb9fa3456c86b7802300ec
jeremy <jeremymartinez11@gmail.com>
2015-04-29 01:29:55 -0700

    small changes

--- myapp/app/assets/javascripts/app/controllers/UsersCtrl.js
+++ myapp/app/assets/javascripts/app/controllers/UsersCtrl.js
@@ -5,6 +5,7 @@
 angular.module('myapp.controllers')
   .controller('UsersController',
     function($scope, UsersService, $q) {
+      $scope.Math = window.Math;
       UsersService.patients().then(function(patients) {
         $scope.patients = [];
         for (var patient in patients) {
